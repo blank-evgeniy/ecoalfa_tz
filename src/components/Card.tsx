324c1ce0a45eaa4beee51445ee3c502a6b80a039
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-08-20 16:09:07 +0200

    The "Like" and "delete" buttons are placed in separate components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 
 import { storeSlice } from '../store/reducers/storeSlice';
 import { useAppDispatch } from '../hooks/reduxHooks';
+import DeleteButton from './DeleteButton';
+import LikeButton from './LikeButton';
 
 interface CardProps {
     data: Item;
@@ -58,41 +60,13 @@ const Card: React.FC<CardProps> = ({ data, favorite }) => {
                         <span className="text-3xl font-bold text-gray-900">
                             ${data.price}
                         </span>
-                        <button
-                            onClick={handleDelete}
-                            className="rounded-md border p-2 text-gray-500 hover:text-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="32"
-                                height="32"
-                                fill="currentColor"
-                                viewBox="0 0 16 16"
-                            >
-                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                            </svg>
-                        </button>
-                        <button
-                            onClick={handleToFavorite}
-                            className={
-                                isFavorite
-                                    ? 'rounded-md border p-2 text-red-500 hover:text-red-800 focus:outline-none focus:ring-4 focus:ring-red-300'
-                                    : 'rounded-md border p-2 text-gray-500 hover:text-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300'
-                            }
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="32"
-                                height="32"
-                                fill="currentColor"
-                                viewBox="0 0 16 16"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
-                                />
-                            </svg>
-                        </button>
+                        <DeleteButton onClick={(e) => handleDelete(e)} />
+                        <LikeButton
+                            onClick={(e) => {
+                                handleToFavorite(e);
+                            }}
+                            isActive={isFavorite}
+                        />
                     </div>
                 </div>
             </Link>
---
+++ src/components/LikeButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface LikeButtonProps {
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    isActive: boolean;
+}
+
+const LikeButton: React.FC<LikeButtonProps> = ({ onClick, isActive }) => {
+    return (
+        <button
+            onClick={onClick}
+            className={
+                isActive
+                    ? 'rounded-md border p-2 text-red-500 hover:text-red-800 focus:outline-none focus:ring-4 focus:ring-red-300'
+                    : 'rounded-md border p-2 text-gray-500 hover:text-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300'
+            }
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+            >
+                <path
+                    fillRule="evenodd"
+                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+                />
+            </svg>
+        </button>
+    );
+};
+
+export default LikeButton;
